c1f1da07a8f843d3336e6b6d55ca04669fb4b428
篠原智哉 <tomoya6032@yahoo.co.jp>
2022-12-08 07:26:42 +0900

    エラー表示発生 ググりながら解決を

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   
-  get '/' => 'toppage#index'
+  # get '/' => 'toppage#index'
+  root to: 'toppage#index'
 
   resources :articles do
-    resources :comments, only: [:new, :show, :edit, :create, :update, :destroy]
+    resources :comments, only: [:index, :new, :show, :edit, :create, :update, :destroy]
   end
 
-  resources :information, only: [:new, :show, :edit, :create, :update, :destroy]
-  resources :chat, only: [:new, :show, :edit, :create, :update, :destroy]
+  resources :information, only: [:index, :new, :show, :edit, :create, :update, :destroy]
+  resources :chat, only: [:index, :new, :show, :edit, :create, :update, :destroy]
   resources :favorites, only: [:index]
   resource :profile, only: [:show, :edit, :update]
   
