f021092657b2b2af8ed26f6ee751001d3cab42b2
篠原智哉 <tomoya6032@yahoo.co.jp>
2022-12-21 07:06:41 +0900

    アバターの下に余白が空いてしまう事象を改善。 プロフィールの編集はできるようになったが、まだ削除でエラーが起こってしまう。

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,8 @@
 class Comment < ApplicationRecord
     belongs_to :article
+    # belongs_to :user
+    # belongs_to :profile
+    # has_one_attached :avatar
     validates :content, presence: true
 
 
