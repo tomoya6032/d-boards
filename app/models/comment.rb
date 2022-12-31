eb6e7e76cb725e2237320e4756a92a80ddb51189
篠原智哉 <tomoya6032@yahoo.co.jp>
2022-12-31 11:23:50 +0900

    チャットの返信機能完成 記事コメント機能完成 次はレクチャー投稿機能とインフォメーション投稿機能を作る

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
     belongs_to :article
-    # belongs_to :user
+    belongs_to :user
     # belongs_to :profile
     has_one_attached :avatar
     validates :content, presence: true
