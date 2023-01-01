2b02d1f8e498372f5ac596f9e3e997f7864d3110
篠原智哉 <tomoya6032@yahoo.co.jp>
2023-01-01 09:02:24 +0900

    今日はインフォメーションボードを作成。 htmlでテキストを入れて、cssで整えて、コントローラーでアクションを定義して、モデルで関連付けをしてDBに保存するカラムを定義。articleのコピーだけど復習になる

--- app/controllers/informations_controller.rb
+++ app/controllers/informations_controller.rb
@@ -1,9 +1,10 @@
 class InformationsController < ApplicationController
 
-
+   before_action :set_information, only: [:show]
+   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
    def index
-
+      @informations = Information.all
 
    end
   
@@ -15,33 +16,56 @@ class InformationsController < ApplicationController
   
   
    def new
-  
+      # @information = Information.new
+      @information = current_user.informations.build
   
    end
   
   
    def create
-  
+      @information = current_user.informations.build(information_params)
+      if @information.save
+        redirect_to information_path(@information), notice: '保存ができたよ'
+      else
+        flash.now[:error] = '保存に失敗しました'
+        render :new
+      end
   
    end
   
   
    def edit
-  
+      @information = current_user.informations.find(params[:id])
   
    end
   
   
    def update
-  
+      @information = current_user.informations.find(params[:id])
+      if @information.update(information_params)
+        redirect_to information_path(@information),notice: '更新できました'
+      else
+        flash.now[:error] = '更新できませんでした'
+        render :edit
+      end
   
    end
   
  
    def destroy
-  
+      information = current_user.articles.find(params[:id])
+      information.destroy!
+      redirect_to root_path, notice: '削除に成功しました'
   
    end
-  
+   
+   # private
+   # def information_params
+   #   params.require(:information).permit(:title, :content, :eyecatch)
+   # end
+
+   def set_information
+     @information = Information.find(params[:id])
+   end
 
 end
--- db/migrate/20221206212018_create_information.rb
+++ db/migrate/20221206212018_create_information.rb
@@ -1,7 +1,9 @@
 class CreateInformation < ActiveRecord::Migration[6.0]
   def change
     create_table :information do |t|
-
+      t.references :user, null: false
+      t.string :title, null: false
+      t.text :content, null: false
       t.timestamps
     end
   end
