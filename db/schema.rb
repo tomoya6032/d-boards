0cc5499fcb56028c05264922437e1099662161b2
篠原智哉 <tomoya6032@yahoo.co.jp>
2023-01-22 08:50:27 +0900

    Revert "oooo"
    This reverts commit ef3b4efb663750d44d2713bc65fb21bd5951f5b0.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_05_211237) do
+ActiveRecord::Schema.define(version: 2023_01_10_200831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -121,6 +121,11 @@ ActiveRecord::Schema.define(version: 2023_01_05_211237) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["chat_id"], name: "index_replies_on_chat_id"
     t.index ["user_id"], name: "index_replies_on_user_id"
+  end
+
+  create_table "user_indices", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
