ef3b4efb663750d44d2713bc65fb21bd5951f5b0
篠原智哉 <tomoya6032@yahoo.co.jp>
2023-01-22 08:50:07 +0900

    oooo

--- app/models/user_indices.rb
+++
@@ -1,2 +0,0 @@
-class UserIndices < ApplicationRecord
-end
--- db/migrate/20230110200831_create_user_indices.rb
+++
@@ -1,8 +0,0 @@
-class CreateUserIndices < ActiveRecord::Migration[6.0]
-  def change
-    create_table :user_indices do |t|
-
-      t.timestamps
-    end
-  end
-end
